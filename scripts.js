698052fd9d12c8a3c6bf69d2b10fc76d71f82f03
maciejberka <maciejberka@gmail.com>
2018-08-03 13:57:17 +0200

    Add animations

--- scripts.js
+++ scripts.js
@@ -2,10 +2,13 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
 hamburger.addEventListener('click', function(){
+    
     if(!nav.classList.contains('nav-visible')){
         nav.classList.add('nav-visible');
+        hamburger.classList.add('hamburger-animation')
     } else {
         nav.classList.remove('nav-visible');
+        hamburger.classList.remove('hamburger-animation')
     }
     
 })
